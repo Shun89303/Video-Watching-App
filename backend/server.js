c6dfd5ebfc5f5a8492b0588be74ce403a81357d8
Shun <ruka4662@gmail.com>
2026-03-12 16:07:17 +0630

    cloud server Render + cloud database MYSQL, working version

--- backend/server.js
+++ backend/server.js
@@ -96,3 +96,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
+
+// Everything works as of right now with cloud server Render + cloud database Railway
