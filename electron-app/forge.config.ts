9169ed0efb984930c0efa80a237d0e467b9d3010
Shun <ruka4662@gmail.com>
2026-03-13 15:12:16 +0630

    forge config preload extra resource path testing

--- electron-app/forge.config.ts
+++ electron-app/forge.config.ts
@@ -10,7 +10,7 @@ import { FuseV1Options, FuseVersion } from "@electron/fuses";
 const config: ForgeConfig = {
 	packagerConfig: {
 		asar: true,
-		extraResource: [".vite/build/preload.js", "assets"],
+		extraResource: ["src/preload.js", "assets"],
 	},
 	rebuildConfig: {},
 	makers: [
