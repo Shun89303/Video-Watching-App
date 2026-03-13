0ad3b1a3e4b168742bafd7ee03cabd15b43a1274
Shun <ruka4662@gmail.com>
2026-03-13 13:29:32 +0630

    production preload file fixed

--- electron-app/vite.preload.config.ts
+++ electron-app/vite.preload.config.ts
@@ -4,8 +4,9 @@ import { resolve } from "path";
 export default defineConfig({
 	build: {
 		// Preload scripts are plain Node (Electron main context), not a browser
-		target: "es2020",
+		target: "esnext",
 		outDir: ".vite/build", // temporary build folder for preload
+		emptyOutDir: true,
 		lib: {
 			entry: resolve(__dirname, "src/preload.ts"),
 			formats: ["cjs"], // preload must be commonjs
