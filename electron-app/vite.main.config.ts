ec0feba8a1541c28379561838c2c227f46573259
Shun <ruka4662@gmail.com>
2026-03-13 15:39:19 +0630

    vite main config testing

--- electron-app/vite.main.config.ts
+++ electron-app/vite.main.config.ts
@@ -3,10 +3,12 @@ import path from "node:path";
 
 export default defineConfig({
 	build: {
+		target: "node14",
 		outDir: path.resolve(__dirname, ".vite/build"),
-		emptyOutDir: true,
+		emptyOutDir: false,
 		rollupOptions: {
 			input: path.resolve(__dirname, "src/main.ts"), // entry point for main process
+			external: ["electron", "url", "path"], // Node built-ins
 			output: {
 				format: "cjs",
 			},
