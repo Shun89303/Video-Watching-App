603c4ea87c1be68311a0ae52f6fd6ae2b21713da
Shun <ruka4662@gmail.com>
2026-03-13 20:10:57 +0630

    renderer rollup input fixed

--- electron-app/config/vite.renderer.config.ts
+++ electron-app/config/vite.renderer.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-	root: "../src/renderer", // folder containing your renderer HTML/JS/CSS
+	root: "../src/renderer/pages", // folder containing your renderer HTML/JS/CSS
 	base: "./", // ensures relative paths work in packaged app
 	build: {
 		outDir: path.resolve(__dirname, "../.vite/build/renderer"),
 		emptyOutDir: true, // clear old builds
 		rollupOptions: {
 			input: {
-				main: "pages/index.html",
-				videoList: "pages/videoList.html",
+				main: "index.html",
+				videoList: "videoList.html",
 			},
 			output: {
 				assetFileNames: "assets/[name][extname]", // put images/videos in assets folder
