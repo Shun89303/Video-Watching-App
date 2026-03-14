593616b9b44c486bf1aba2c03f634f372ab72348
Shun <ruka4662@gmail.com>
2026-03-14 10:41:39 +0630

    prod db testing

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -24,11 +24,16 @@ const logger =
 
 // MySQL connection
 const db = mysql.createConnection({
-	host: process.env.MYSQLHOST || "localhost",
-	user: process.env.MYSQLUSER || "root",
-	password: process.env.MYSQLPASSWORD || "root1234",
-	database: process.env.MYSQLDATABASE || "video_app",
-	port: process.env.MYSQLPORT || 3306,
+	host: process.env.MYSQLHOST,
+	user: process.env.MYSQLUSER,
+	password: process.env.MYSQLPASSWORD,
+	database: process.env.MYSQLDATABASE,
+	port: process.env.MYSQLPORT,
+	// host: process.env.MYSQLHOST || "localhost",
+	// user: process.env.MYSQLUSER || "root",
+	// password: process.env.MYSQLPASSWORD || "root1234",
+	// database: process.env.MYSQLDATABASE || "video_app",
+	// port: process.env.MYSQLPORT || 3306,
 });
 
 // Promise-based startup check
