0876f49e487030de50658f6df0e9da77778e7afa
Shun <ruka4662@gmail.com>
2026-03-14 10:55:42 +0630

    db connection explicit testing

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -24,11 +24,16 @@ const logger =
 
 // MySQL connection
 const db = mysql.createConnection({
-	host: process.env.MYSQLHOST,
-	user: process.env.MYSQLUSER,
-	password: process.env.MYSQLPASSWORD,
-	database: process.env.MYSQLDATABASE,
-	port: process.env.MYSQLPORT,
+	// host: process.env.MYSQLHOST,
+	// user: process.env.MYSQLUSER,
+	// password: process.env.MYSQLPASSWORD,
+	// database: process.env.MYSQLDATABASE,
+	// port: process.env.MYSQLPORT,
+	host: "ballast.proxy.rlwy.net",
+	user: "root",
+	password: "qkfSLfOeJeFkfscCPJlQmxsZrBGWKzCs",
+	database: "railway",
+	port: 40898,
 	// host: process.env.MYSQLHOST || "localhost",
 	// user: process.env.MYSQLUSER || "root",
 	// password: process.env.MYSQLPASSWORD || "root1234",
